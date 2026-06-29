d07fa2d36466bb5cd8efe68779dd2e2679999aa0
thea <layamgracielle@gmail.com>
2026-06-29 18:11:02 +0800

    fixed the error in the record a payment

---
+++ backend/api/management/commands/fix_sequences.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.core.management.color import no_style
+from django.db import connection
+from api.models import Transaction, Consignee, Expense, RevenueRecipient
+
+class Command(BaseCommand):
+    help = 'Fixes database sequences'
+
+    def handle(self, *args, **options):
+        sequence_sql = connection.ops.sequence_reset_sql(no_style(), [Transaction, Consignee, Expense, RevenueRecipient])
+        with connection.cursor() as cursor:
+            for sql in sequence_sql:
+                cursor.execute(sql)
+        self.stdout.write(self.style.SUCCESS('Successfully reset database sequences'))
