de6d2a0750a8948ba1e2f027856c44b6f5c4263b
Gracielle Mieh C. Layam <layamgracielle@gmail.com>
2026-07-06 22:48:00 +0800

    fixed sequences

--- backend/api/management/commands/fix_sequences.py
+++ backend/api/management/commands/fix_sequences.py
@@ -1,14 +1,15 @@
 from django.core.management.base import BaseCommand
 from django.core.management.color import no_style
 from django.db import connection
-from api.models import Transaction, Consignee, Expense, RevenueRecipient, Liquidation
+from api.models import Transaction, Consignee, Expense, RevenueRecipient, Liquidation, CashOnBank
 
 class Command(BaseCommand):
     help = 'Fixes database sequences'
 
     def handle(self, *args, **options):
-        sequence_sql = connection.ops.sequence_reset_sql(no_style(), [Transaction, Consignee, Expense, RevenueRecipient, Liquidation])
+        sequence_sql = connection.ops.sequence_reset_sql(no_style(), [Transaction, Consignee, Expense, RevenueRecipient, Liquidation, CashOnBank])
         with connection.cursor() as cursor:
             for sql in sequence_sql:
                 cursor.execute(sql)
         self.stdout.write(self.style.SUCCESS('Successfully reset database sequences'))
+
